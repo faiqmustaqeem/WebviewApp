305dd7777c85c660a89cee15b86a5b6d9eaf918b
faiq <faiq@faiqs-Mac.local>
2018-08-26 09:23:35 -0700

    app completed

--- Unique Innovator/MainScreenViewController.swift
+++ Unique Innovator/MainScreenViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainScreenViewController.swift
 //  Unique Innovator
 //
 //  Created by faiq on 21/08/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Unique Innovator/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  
+//
+//  Created by faiq on 21/08/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
